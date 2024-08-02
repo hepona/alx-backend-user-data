9db1e7158e1d2044337d8abde077e92f3b0cb323
hepona <hepona10@gmail.com>
2024-08-02 01:38:25 +0100

    test

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""5. Encrypting passwords"""
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """returns a salted, hashed password, which is a byte string."""
+    pw_bite = password.encode()
+    return bcrypt.hashpw(pw_bite, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str):
+    """validate that the provided password matches the hashed password."""
+    return bcrypt.checkpw(password.encode(), hashed_password)
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,7 +10,25 @@ def filter_datum(
 ) -> str:
     """returns the log message obfuscated"""
     for f in fields:
-        message = re.sub(rf"{f}=.*?{separator}",
-                         f"{f}={redaction}{separator}", message)
+        message = re.sub(rf"{f}=.*?{separator}", f"{f}={redaction}{separator}", message)
 
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records"""
+        logging.basicConfig(level=logging.DEBUG, format=record)
+        logger = logging.getLogger()
+        logger.info(record)
+        # filter_datum(self.field)
