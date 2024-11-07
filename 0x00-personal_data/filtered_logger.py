2360b286a1659b531cfb8ecae1a5a80bd484097b
hepona <hepona10@gmail.com>
2024-11-07 09:10:29 +0100

    0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,8 +10,8 @@ def filter_datum(
 ) -> str:
     """returns the log message obfuscated"""
     for f in fields:
-        message = re.sub(rf"{f}=.*?{separator}",\
-            f"{f}={redaction}{separator}", message)
+        message = re.sub(
+            rf"{f}=.*?{separator}", f"{f}={redaction}{separator}", message)
 
     return message
 
