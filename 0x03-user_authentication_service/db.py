9a78f54834f8155e85e1db4b6ab04e7cfa994c6e
hepona <hepona10@gmail.com>
2024-11-21 16:20:07 +0100

    2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -52,6 +52,6 @@ class DB:
         user = self.find_user_by(id=user_id)
         for k, v in kwargs.items():
             if not hasattr(user, k):
-                raise ValueError()
+                raise ValueError
             setattr(user, k, v)
         self._session.commit()
