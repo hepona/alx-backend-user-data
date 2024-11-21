b20db7ae6c8fe862c370fb7a04056d4e89effcd2
hepona <hepona10@gmail.com>
2024-11-21 11:24:14 +0100

    1

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,7 +11,7 @@ class User(Base):
     """SQLAlchemy model named User for a database table named users"""
 
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
