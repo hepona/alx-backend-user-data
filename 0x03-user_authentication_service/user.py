cac5306085c7784df2b48b48d4891319ed21d938
hepona <hepona10@gmail.com>
2024-11-21 10:48:32 +0100

    0

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" User model"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """class user"""
+
+    __tablename__ = "Users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
