c080caa9e40615fa5115f9c92ddffd14ad9dccc4
hepona <hepona10@gmail.com>
2024-11-22 00:33:01 +0100

    7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
 from flask import Flask, jsonify
+from auth import Auth
 
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -10,6 +13,16 @@ app = Flask(__name__)
 def home():
     """return JSON payload"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users(email, password):
+    """register user if not exist"""
+
+    if AUTH.register_user(email, password):
+        return jsonify({f"email": "{email}", "message": "user created"})
+    else:
+        return jsonify({"message": "email already registered"}, 400)
 
 
 if __name__ == "__main__":
