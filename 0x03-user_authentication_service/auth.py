090fb8868b8b6692370175f50740e22bdc8a0257
hepona <hepona10@gmail.com>
2024-11-21 23:56:01 +0100

    4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Authentification"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """take pwd and return bytes"""
+    byte = password.encode("utf-8")
+    hash = bcrypt.hashpw(byte, bcrypt.gensalt())
+    return hash
