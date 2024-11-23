be65d0e69abca07aba3b4bef9223195a39552ff6
hepona <hepona10@gmail.com>
2024-11-23 21:46:28 +0100

    14  done

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -47,7 +47,7 @@ def logout():
     user = AUTH.get_user_from_session_id(session_id=session_id)
     if user:
         AUTH.destroy_session(user.id)
-        return redirect("/", 302)
+        return redirect("/home", 302)
     abort(403)
 
 
