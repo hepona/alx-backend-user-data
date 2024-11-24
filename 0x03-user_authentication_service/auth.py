0589df75b0957dfe867051867ccbacc0b90f9ee6
hepona <hepona10@gmail.com>
2024-11-24 02:12:25 +0100

    18 fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -37,7 +37,7 @@ class Auth:
             return user
 
     def valid_login(self, email: str, password: str) -> bool:
-        """ """
+        """check if the login is valide or not"""
         try:
             user = self._db.find_user_by(email=email)
             return bcrypt.checkpw(password.encode(), user.hashed_password)
