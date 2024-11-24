b04379575030b15ffeb2cae938989f46057d27f4
hepona <hepona10@gmail.com>
2024-11-24 01:12:17 +0100

    16 fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,10 +69,11 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """Generate reset password token"""
-        user = self._db.find_user_by(email)
         try:
-            token = _generate_uuid()
-            self._db.update_user(user.id, reset_token=token)
-            return token
-        except ValueError:
-            raise
+            user = self._db.find_user_by(email)
+        except NoResultFound:
+            raise ValueError
+
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
