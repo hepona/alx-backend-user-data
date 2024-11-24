03cc5a868ee727a7b7b0b27add02bb1a5e538ac3
hepona <hepona10@gmail.com>
2024-11-24 01:24:54 +0100

    16 fix

---
+++ 0x03-user_authentication_service/16-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import uuid
+from unittest import TestCase, mock
+from auth import Auth
+from db import DB
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,10 +69,10 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """Generate reset password token"""
-        user = self._db.find_user_by(email)
         try:
-            token = _generate_uuid()
-            self._db.update_user(user.id, reset_token=token)
-            return token
+            user = self._db.find_user_by(email = email)
         except ValueError:
             raise
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
