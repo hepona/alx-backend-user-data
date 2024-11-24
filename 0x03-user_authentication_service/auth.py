1bd015ea32d73bc8fdd7a78bf8a4b74775f083d8
hepona <hepona10@gmail.com>
2024-11-24 01:09:26 +0100

    16 fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,3 +66,13 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """destroy a session"""
         self._db.update_user(user_id=user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate reset password token"""
+        user = self._db.find_user_by(email)
+        try:
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except ValueError:
+            raise
